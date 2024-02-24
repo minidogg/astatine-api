219199639cc7cfeefc631d0a398d1d75b1b0c26a
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-02-24 16:38:35 -0600

    a

--- server/creation.js
+++ server/creation.js
@@ -2,8 +2,25 @@ const mongoose = require('mongoose');
 const schema = require("./schema.js")
 
 
-function createUser(name,){
+async function createUser(username,password,email){
+    if(await schema.User.find({ username: username })[0]){
+        return {error:"Account already exists"}
+    }
+    if(/[^\w]/g.test(username)){
+        return {error:"Invalid username"}
+    }
+    if(username.length<=3||username.length>25){
+        return {error:"Invalid username length"}
+    }
+    if(password.length<8||password.length>100){
+        return {error:"Invalid password length"}
+    }
+    if(!/[a-z]/g.test(password)||!/[A-Z]/g.test(password)||!/\W/g.test(password)){
+        return {error:"Invalid password"}
+    }
+    console.log("E")
 
+    return {}
 }
 
 
--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 (async()=>{
     //require mongo stuff
     const schema = require("./schema.js")
+    const creationStuff = require("./creation.js")
     const mongoose = require('mongoose');
     const path = require("path")
+    
 
     //mongo connect
     var connection = await mongoose.connect('mongodb://127.0.0.1:27017/chat')
@@ -14,14 +16,34 @@
 
     const wss = new WebSocketServer({ port: 8080 });
     
+    var sessions = {}
     wss.on('connection', function connection(ws) {
+      var session = {}
       ws.on('error', console.error);
     
-      ws.on('message', function message(data) {
-        console.log('received: %s', data);
+      ws.on('message', async function message(dataa) {
+        try{
+        var data = JSON.parse(dataa)
+        
+        if(data.type=="createUser"){
+            let acc = await creationStuff.createUser(data.username,data.password,data.email)
+
+            if(acc.hasOwnProperty("error")){
+                ws.send(JSON.stringify({"error":acc.error,"status":409}))
+                return
+            }
+            ws.send(JSON.stringify({"success":"Made account succesfuly!"}))
+        }else{
+            ws.send(JSON.stringify({"error":"Invalid message type","status":404}))
+        }
+
+    }catch(err){
+        console.warn(err)
+        ws.send(JSON.stringify({"error":"Something went wrong on the server side.","status":500}))
+    }
       });
     
-      ws.send('something');
+
     });
 
 })()
